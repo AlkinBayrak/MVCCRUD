d8c65b335575e7fb72c7136d46d80653d66e4875
AlkinBayrak <alkinbayrak01@gmail.com>
2024-10-31 16:56:08 +0300

    CRUD işlemler yapıldı

--- MVCCRUD/Controllers/SiparisController.cs
+++ MVCCRUD/Controllers/SiparisController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using MVCCRUD.Context;
 using MVCCRUD.Entities.Concrete;
@@ -19,11 +20,49 @@ namespace MVCCRUD.Controllers
             return View(model);
         }
 
+        [HttpGet]
+        public IActionResult Create()
+        {
+            ViewBag.UrunId = new SelectList(_context.Urunler, "Id", "UrunAdi");
+            ViewBag.MusteriId = new SelectList(_context.Musteriler, "Id", "Ad");
+
+            return View();
+        }
+
         [HttpPost]
         public IActionResult Create(Siparis siparis)
         {
             _context.Siparisler.Add(siparis);
             _context.SaveChanges();
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Edit(int id)
+        {
+            Siparis siparis = _context.Siparisler.Where(s => s.Id == id).FirstOrDefault();
+            ViewBag.mSiparis = new SelectList(_context.Musteriler, "Id", "Ad", siparis.MusteriId);
+            ViewBag.uSiparis = new SelectList(_context.Urunler, "Id", "UrunAdi", siparis.UrunId);
+
+            return View(siparis);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Siparis siparis)
+        {
+            _context.Siparisler.Update(siparis);
+            _context.SaveChanges();
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Delete(int id)
+        {
+            Siparis siparis = _context.Siparisler.Where(c => c.Id == id).FirstOrDefault();
+
+            _context.Siparisler.Remove(siparis);
+            _context.SaveChanges();
+
 
             return RedirectToAction(nameof(Index));
         }
---
+++ MVCCRUD/Controllers/UrunController.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVCCRUD.Context;
+using MVCCRUD.Entities.Concrete;
+
+namespace MVCCRUD.Controllers
+{
+    public class UrunController : Controller
+    {
+        private readonly MyDbContext _context;
+
+        public UrunController(MyDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            var model = _context.Urunler.ToList();
+
+            return View(model);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            
+
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Urun urun)
+        {
+            _context.Urunler.Add(urun);
+            _context.SaveChanges();
+
+            return RedirectToAction(nameof(Index));
+        }
+
+
+        public IActionResult Edit(int id)
+        {
+            Urun urun = _context.Urunler.Where(c => c.Id == id).FirstOrDefault();
+
+
+            return View(urun);
+        }
+
+        [HttpPost]
+        public IActionResult Update(Urun urun)
+        {
+
+            _context.Urunler.Update(urun);
+            _context.SaveChanges();
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Delete(int id)
+        {
+            Urun urun = _context.Urunler.Where(c => c.Id == id).FirstOrDefault();
+
+            _context.Urunler.Remove(urun);
+            _context.SaveChanges();
+
+
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
